de8dcf59b142fc090b99f2c0fa7d8f3e8566911f
Pengyu Zhang <r1016626@pc-klas2-8.esat.kuleuven.be>
2025-04-16 15:16:58 +0200

    basic pipeline for cpu.v

--- SIM/xcelium.d/run.lnx8664.23.03.d/.timestamp.ts
+++ SIM/xcelium.d/run.lnx8664.23.03.d/.timestamp.ts
@@ -1,10 +1,12 @@
 1741156226 /users/students/r1016447/Documents/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/cpu.v
 1741156226 /users/students/r1016447/Linux/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/register_file.v
+1743588069 /users/students/r1016626/Downloads/Computer-Archi/Verilog/sky130_sram_2rw.v
 1741156226 /users/students/r1016447/Documents/CA/Design_Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/reg_arstn.v
 1741156226 /users/students/r1016447/Documents/CA/Design_Files/CA_Exercise_2025/CA_Exercises/Verilog/cpu_tb.v
 1741156226 /users/students/r1016447/Linux/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/reg_arstn.v
 1741156226 /users/students/r1016447/Linux/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/cpu.v
 1741156226 /users/students/r1016447/Linux/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/alu_control.v
+1743588069 /users/students/r1016626/Downloads/Computer-Archi/Verilog/RTL/mux_2.v
 1741156226 /users/students/r1016447/Documents/CA/Design_Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/register_file.v
 1741156226 /users/students/r1016447/Documents/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/reg_arstn.v
 1741156226 /users/students/r1016447/Documents/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/branch_unit.v
@@ -12,18 +14,27 @@
 1741156226 /users/students/r1016447/Documents/CA/Design_Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/pc.v
 1743583792 /users/students/r1016447/Documents/CA/Design_Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/alu_control.v
 1741156226 /users/students/r1016447/Documents/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/sky130_sram_2rw.v
+1743588176 /users/students/r1016626/Downloads/Computer-Archi/Verilog/RTL/alu.v
 1741156226 /users/students/r1016447/Documents/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/mux_2.v
+1743588069 /users/students/r1016626/Downloads/Computer-Archi/Verilog/RTL/immediate_extend_unit.v
 1743583792 /users/students/r1016447/Documents/CA/Design_Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/control_unit.v
 1741156226 /users/students/r1016447/Linux/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/cpu_tb.v
+1743588069 /users/students/r1016626/Downloads/Computer-Archi/Verilog/RTL/reg_arstn.v
 1741156226 /users/students/r1016447/Documents/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/alu.v
+1743588069 /users/students/r1016626/Downloads/Computer-Archi/Verilog/cpu_tb.v
 1741156226 /users/students/r1016447/Documents/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/control_unit.v
 1741156226 /users/students/r1016447/Linux/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/control_unit.v
 1743584706 /users/students/r1016447/Documents/CA/Design_Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/cpu.v
 1741156226 /users/students/r1016447/Documents/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/cpu_tb.v
 1741156226 /users/students/r1016447/Linux/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/alu.v
+1743588176 /users/students/r1016626/Downloads/Computer-Archi/Verilog/RTL/alu_control.v
 1741156226 /users/students/r1016447/Linux/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/pc.v
 1741156226 /users/students/r1016447/Documents/CA/Design_Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/mux_2.v
+1743588176 /users/students/r1016626/Downloads/Computer-Archi/Verilog/RTL/control_unit.v
+1743588069 /users/students/r1016626/Downloads/Computer-Archi/Verilog/RTL/pc.v
 1741156226 /users/students/r1016447/Linux/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/branch_unit.v
+1743588069 /users/students/r1016626/Downloads/Computer-Archi/Verilog/RTL/register_file.v
+1743588069 /users/students/r1016626/Downloads/Computer-Archi/Verilog/RTL/sram.v
 1741156226 /users/students/r1016447/Linux/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/sky130_sram_2rw.v
 1741156226 /users/students/r1016447/Documents/CA/Design_Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/reg_arstn_en.v
 1741156226 /users/students/r1016447/Linux/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/immediate_extend_unit.v
@@ -38,5 +49,8 @@
 1741156226 /users/students/r1016447/Documents/CA/Design_Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/sram.v
 1743583792 /users/students/r1016447/Documents/CA/Design_Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/alu.v
 1741156226 /users/students/r1016447/Documents/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/register_file.v
+1743588069 /users/students/r1016626/Downloads/Computer-Archi/Verilog/RTL/reg_arstn_en.v
 1741156226 /users/students/r1016447/Documents/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/sram.v
+1744806182 /users/students/r1016626/Downloads/Computer-Archi/Verilog/RTL/cpu.v
 1741156226 /users/students/r1016447/Linux/Computer Architecture/Design Files/CA_Exercise_2025/CA_Exercises/Verilog/RTL/sram.v
+1743588069 /users/students/r1016626/Downloads/Computer-Archi/Verilog/RTL/branch_unit.v
---
+++ Verilog/RTL_SOLUTION3_pipeline_basic_MULT2/alu_control.v
@@ -0,0 +1,53 @@
+module alu_control(
+      input wire [6:0] funct7,
+      input wire [2:0] func3,
+      input wire [1:0] alu_op,
+      input wire       mult,
+      output reg [3:0] alu_control
+   );
+
+   // ALUOp 对应编码
+   parameter [1:0] ADD_OPCODE    = 2'b00;
+   parameter [1:0] SUB_OPCODE    = 2'b01;
+   parameter [1:0] R_TYPE_OPCODE = 2'b10;
+
+   // ALU 控制码定义（参考书中描述）
+   parameter [3:0] AND_OP        = 4'd0;
+   parameter [3:0] OR_OP         = 4'd1;
+   parameter [3:0] ADD_OP        = 4'd2;
+   parameter [3:0] SLL_OP        = 4'd3;
+   parameter [3:0] SRL_OP        = 4'd4;
+   parameter [3:0] SUB_OP        = 4'd6;
+   parameter [3:0] SLT_OP        = 4'd7;
+   parameter [3:0] MUL_OP        = 4'd8;  // 新的乘法操作
+
+   reg [3:0] rtype_op;
+   
+   always @(*) begin
+      if(mult)
+         rtype_op = MUL_OP;
+      else begin
+         case({funct7[5], func3})
+            4'b0000: rtype_op = ADD_OP;  // add
+            4'b1000: rtype_op = SUB_OP;  // sub
+            4'b0111: rtype_op = AND_OP;  // and
+            4'b0110: rtype_op = OR_OP;   // or
+            4'b0010: rtype_op = SLT_OP;  // slt
+            4'b0001: rtype_op = SLL_OP;  // sll
+            4'b0101: rtype_op = SRL_OP;  // srl
+            default: rtype_op = 4'd0;
+         endcase
+      end
+   end
+
+   always @(*) begin
+      case(alu_op)
+         ADD_OPCODE    : alu_control = ADD_OP;   // 对于 ALU_I、LOAD、STORE
+         SUB_OPCODE    : alu_control = SUB_OP;   // 对于 BRANCH_EQ
+         R_TYPE_OPCODE : alu_control = rtype_op;
+         default       : alu_control = 4'b0;
+      endcase
+   end
+
+endmodule
+
