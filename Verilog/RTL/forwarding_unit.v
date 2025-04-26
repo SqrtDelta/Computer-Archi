9070cad47712b555eb4f3ffc7da4a3db3c7fe543
kai <kai.alpha@outlook.com>
2025-04-26 22:50:57 +0200

    opt forward unit

--- Verilog/RTL/forwarding_unit.v
+++ Verilog/RTL/forwarding_unit.v
@@ -14,15 +14,21 @@ module forwarding_unit(
     always @(*) begin
         forwardA = 2'b00;
         forwardB = 2'b00;
+
+        // MEM/WB 阶段一律可转发（包括 Load 结果）
+        //  上上命令有写入          地址非零
+        if (reg_write_MEM_WB && (rd_MEM_WB != 5'b0)) begin
+            //   非上个指令
+            if (rd_MEM_WB == rs1_ID_EX) forwardA = 2'b10;
+            if (rd_MEM_WB == rs2_ID_EX) forwardB = 2'b10;
+        end
+
         // 只对非 Load 的 EX/MEM 做转发
+        //  上个命令有写入         上个命令非ld          非零地址
         if (reg_write_EX_MEM && !mem_read_EX_MEM && (rd_EX_MEM != 5'b0)) begin
             if (rd_EX_MEM == rs1_ID_EX) forwardA = 2'b01;
             if (rd_EX_MEM == rs2_ID_EX) forwardB = 2'b01;
         end
-        // MEM/WB 阶段一律可转发（包括 Load 结果）
-        if (reg_write_MEM_WB && (rd_MEM_WB != 5'b0)) begin
-            if ((forwardA == 2'b00) && (rd_MEM_WB == rs1_ID_EX)) forwardA = 2'b10;
-            if ((forwardB == 2'b00) && (rd_MEM_WB == rs2_ID_EX)) forwardB = 2'b10;
-        end
+        
     end
 endmodule
